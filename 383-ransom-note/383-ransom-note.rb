8a593cb7b543e27f0363982f446c1e07a62b6485
v12v0 <94933328+v12v0@users.noreply.github.com>
2022-05-25 12:50:57 -0500

    Time: 198 ms (51.54%), Space: 211.9 MB (90.77%) - LeetHub

--- 383-ransom-note/383-ransom-note.rb
+++ 383-ransom-note/383-ransom-note.rb
@@ -3,16 +3,17 @@
 # @return {Boolean}
 def can_construct(ransom_note, magazine)
      note_hash = Hash.new(0)
-    magazine_hash =Hash.new(0)
+    magazine_hash = Hash.new(0)
     
     ransom_note.each_char do |char|
-        note_hash[char] +=1
-    end
+        note_hash[char] +=1 
+    end 
     magazine.each_char do |char|
-       magazine_hash[char] +=1
-    end
+        magazine_hash[char]+=1
+    end 
+    
     note_hash.keys.each do |key|
-        return false if note_hash[key]>magazine_hash[key]
+        return false if note_hash[key] > magazine_hash[key]
     end
     true
 end
