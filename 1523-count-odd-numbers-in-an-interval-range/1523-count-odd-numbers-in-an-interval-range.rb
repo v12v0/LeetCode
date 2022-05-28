c2664948f3349874b4f814e033295987c0e595e6
v12v0 <94933328+v12v0@users.noreply.github.com>
2022-05-28 11:50:04 -0500

    Time: 107 ms (51.95%), Space: 211 MB (37.66%) - LeetHub

--- 1523-count-odd-numbers-in-an-interval-range/1523-count-odd-numbers-in-an-interval-range.rb
+++ 1523-count-odd-numbers-in-an-interval-range/1523-count-odd-numbers-in-an-interval-range.rb
@@ -2,5 +2,5 @@
 # @param {Integer} high
 # @return {Integer}
 def count_odds(low, high)
-   low.even? && high.even? ? (high - low)/2 : ((high - low)/2) + 1
+  low.even? && high.even? ? ((high-low) / 2 )  : ( ((high-low) )  / 2 ) + 1
 end
