a75be79e4c650555b3916471abfa2f24cb6518cd
v12v0 <94933328+v12v0@users.noreply.github.com>
2022-05-30 10:24:05 -0500

    Time: 158 ms (17.56%), Space: 214 MB (12.38%) - LeetHub

---
+++ 704-binary-search/704-binary-search.rb
@@ -0,0 +1,8 @@
+# @param {Integer[]} nums
+# @param {Integer} target
+# @return {Integer}
+def search(nums, target)
+    nums.sort! 
+    nums.include?(target) ? nums.find_index(target) : -1
+    
+end
