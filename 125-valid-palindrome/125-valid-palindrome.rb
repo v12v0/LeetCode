6596217482c723fd0ea9cd4ba634ee5b64f0e0d6
v12v0 <94933328+v12v0@users.noreply.github.com>
2022-06-01 14:37:56 -0500

    Time: 149 ms (60.88%), Space: 213 MB (28.53%) - LeetHub

--- 125-valid-palindrome/125-valid-palindrome.rb
+++ 125-valid-palindrome/125-valid-palindrome.rb
@@ -1,6 +1,6 @@
 # @param {String} s
 # @return {Boolean}
 def is_palindrome(s)
-  s.downcase.gsub(/[^a-zA-Z0-9]/, "").reverse ==   s.downcase.gsub(/[^a-zA-Z0-9]/, "")
+  s.downcase.gsub(/[^a-zA-Z0-9]/, "").reverse ==  s.downcase.gsub(/[^a-zA-Z0-9]/, "")
 
 end
