d2c3d112134087f94e334a3a3983474b5ff3e47a
v12v0 <94933328+v12v0@users.noreply.github.com>
2022-06-01 16:59:54 -0500

    Time: 123 ms (89.31%), Space: 212.5 MB (65.65%) - LeetHub

--- 383-ransom-note/383-ransom-note.rb
+++ 383-ransom-note/383-ransom-note.rb
@@ -2,9 +2,8 @@
 # @param {String} magazine
 # @return {Boolean}
 def can_construct(ransom_note, magazine)
-    ransom_note.chars.uniq.none? do |char|
-        ransom_note.count(char) > magazine.count(char)
+    ransom_note.chars.uniq.none? do |c|
+        ransom_note.count(c) > magazine.count(c)
     end 
-end
     
- 
+end
