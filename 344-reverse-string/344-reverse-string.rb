a946114cbf285b514ade8d2d8d627f1e7145c38d
v12v0 <94933328+v12v0@users.noreply.github.com>
2022-06-02 15:54:36 -0500

    Time: 274 ms (81.15%), Space: 225.2 MB (95.08%) - LeetHub

--- 344-reverse-string/344-reverse-string.rb
+++ 344-reverse-string/344-reverse-string.rb
@@ -1,5 +1,14 @@
 # @param {Character[]} s
 # @return {Void} Do not return anything, modify s in-place instead.
 def reverse_string(s)
-     return s.reverse!
+    left = 0 
+    right = s.length - 1
+    
+    while left < right do 
+        s[left], s[right] = s[right] , s[left]
+        left += 1 
+        right -=1
+    end 
+  
 end
+left = 0
