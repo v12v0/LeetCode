b0fbcfe5887710f443760c6cce5a6e4d22c9f301
v12v0 <94933328+v12v0@users.noreply.github.com>
2022-06-10 14:41:58 -0500

    Time: 91 ms (72.33%), Space: 211.1 MB (13.21%) - LeetHub

---
+++ 876-middle-of-the-linked-list/876-middle-of-the-linked-list.rb
@@ -0,0 +1,20 @@
+# Definition for singly-linked list.
+# class ListNode
+#     attr_accessor :val, :next
+#     def initialize(val = 0, _next = nil)
+#         @val = val
+#         @next = _next
+#     end
+# end
+# @param {ListNode} head
+# @return {ListNode}
+def middle_node(head)
+    slow = fast = head 
+    
+    while ( fast != nil && fast.next != nil)
+        slow = slow.next 
+        fast = fast.next.next
+    end 
+    slow 
+    
+end
