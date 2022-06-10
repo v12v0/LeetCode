556845e33ce3e7f362bedf6b0dcd3faf9cbae0f3
v12v0 <94933328+v12v0@users.noreply.github.com>
2022-06-10 17:13:34 -0500

    Time: 166 ms (88.40%), Space: 220.5 MB (13.26%) - LeetHub

--- 53-maximum-subarray/53-maximum-subarray.rb
+++ 53-maximum-subarray/53-maximum-subarray.rb
@@ -11,15 +11,12 @@
 
 def max_sub_array(nums)
     current_sum = nums[0]
-    current_max = current_sum
+    current_max = current_sum 
     
-    return nums[0] if nums.length == 1 
-    
-    nums[1..-1].each do |n|
-        current_sum = [current_sum + n, n].max 
-        current_max = [current_sum, current_max].max
-        
+    nums[1..-1].each do |num|
+        current_sum = [current_sum + num , num].max
+        current_max = [current_max, current_sum].max
     end 
-    
     current_max
 end
+
