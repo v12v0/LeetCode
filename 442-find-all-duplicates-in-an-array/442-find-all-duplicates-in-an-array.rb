71f4b29925abf2f12b7c867d98d9ff08df01ffeb
v12v0 <94933328+v12v0@users.noreply.github.com>
2022-06-11 15:20:32 -0500

    Time: 238 ms (33.33%), Space: 222.7 MB (22.22%) - LeetHub

--- 442-find-all-duplicates-in-an-array/442-find-all-duplicates-in-an-array.rb
+++ 442-find-all-duplicates-in-an-array/442-find-all-duplicates-in-an-array.rb
@@ -2,9 +2,7 @@
 # @return {Integer[]}
 def find_duplicates(nums)
     hash = Hash.new(0)
+    nums.each {|num| hash[num] += 1}
+    hash.keys.select{|key,value| hash[key] > 1}
     
-    nums.each do |num|
-        hash[num] += 1
-    end 
-     hash.keys.select{|key,val| hash[key] > 1}
 end
