f0e35c77eeb4eab8d775d2db76ee040362ef62d7
v12v0 <94933328+v12v0@users.noreply.github.com>
2022-06-11 09:37:26 -0500

    Time: 165 ms (22.84%), Space: 213 MB (52.28%) - LeetHub

--- 136-single-number/136-single-number.rb
+++ 136-single-number/136-single-number.rb
@@ -1,14 +1,5 @@
 # @param {Integer[]} nums
 # @return {Integer}
 def single_number(nums)
-    hash = Hash.new
-    
-    nums.each do |n|
-        if hash[n]
-            hash.delete(n)
-        else 
-            hash[n] = true
-        end 
-    end 
-    p hash.keys.first
+    2 * nums.uniq.sum - nums.sum
 end
