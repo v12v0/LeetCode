112260c2958b380482c05e2dc506578ad83e5e2a
v12v0 <94933328+v12v0@users.noreply.github.com>
2022-06-12 12:10:35 -0500

    Time: 125 ms (38.80%), Space: 211.2 MB (68.40%) - LeetHub

---
+++ 206-reverse-linked-list/206-reverse-linked-list.rb
@@ -0,0 +1,21 @@
+# Definition for singly-linked list.
+# class ListNode
+#     attr_accessor :val, :next
+#     def initialize(val = 0, _next = nil)
+#         @val = val
+#         @next = _next
+#     end
+# end
+# @param {ListNode} head
+# @return {ListNode}
+def reverse_list(head)
+    prev , curr = nil , head 
+    
+    while curr != nil
+        nxt = curr.next
+        curr.next = prev
+        prev = curr
+        curr = nxt
+    end 
+    prev
+end
