5ed871d6e83cefc00791ee311a8da85985817bb1
v12v0 <94933328+v12v0@users.noreply.github.com>
2022-06-22 15:38:22 -0500

    Time: 161 ms (58.90%), Space: 219 MB (20.55%) - LeetHub

--- 217-contains-duplicate/217-contains-duplicate.rb
+++ 217-contains-duplicate/217-contains-duplicate.rb
@@ -1,6 +1,10 @@
 # @param {Integer[]} nums
 # @return {Boolean}
 def contains_duplicate(nums)
-  nums.uniq.length != nums.length
-    
+hash = {}
+    nums.each do |x|
+        return true if hash[x] == x
+        hash[x] = x;
+    end
+    false
 end
